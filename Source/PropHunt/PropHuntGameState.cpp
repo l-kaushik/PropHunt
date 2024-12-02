6fe0cfa13689434ec04684ead28ca9670301c1c7
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-12-02 18:40:19 +0530

    Implemented variables and list needed in player spawn time.
    - PlayerControllerList -> list of players joined the game.
    - HunterList -> current number of hunters in the game.
    - bHasGameStarted -> check if game has started yet or not.
    - MinPlayerNum -> minimum number of players need to start the game.
    - HunterStartLocation -> hunter's spawn location.
    
    - Replicating PlayerControllerList, HunterList and bHasGameStarted
    - Implemented method to add controllers in both list.

--- Source/PropHunt/PropHuntGameState.cpp
+++ Source/PropHunt/PropHuntGameState.cpp
@@ -2,4 +2,39 @@
 
 
 #include "PropHuntGameState.h"
+#include "PropHuntPlayerController.h"
+#include "Net/UnrealNetwork.h"
 
+APropHuntGameState::APropHuntGameState() 
+{
+	bHasGameStarted = false;
+	MinPlayerNum = 2;
+	HunterStartLocation.Set(606.749038, 1241.206218, 99.227695);	// for test purposes only
+}
+
+void APropHuntGameState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(APropHuntGameState, PlayerControllerList);
+	DOREPLIFETIME(APropHuntGameState, bHasGameStarted);
+	DOREPLIFETIME(APropHuntGameState, HunterList);
+}
+
+void APropHuntGameState::AddPlayerController(APropHuntPlayerController* NewController)
+{
+	if (NewController) {
+		PlayerControllerList.AddUnique(NewController);
+	}
+}
+
+TArray<APropHuntPlayerController*> APropHuntGameState::GetPlayerControllerList() const
+{
+	return PlayerControllerList;
+}
+
+void APropHuntGameState::AddHunter(APropHuntPlayerController* NewHunter) {
+	if (NewHunter) {
+		HunterList.AddUnique(NewHunter);
+	}
+}
--- Source/PropHunt/PropHuntGameState.h
+++ Source/PropHunt/PropHuntGameState.h
@@ -6,6 +6,8 @@
 #include "GameFramework/GameStateBase.h"
 #include "PropHuntGameState.generated.h"
 
+
+class APropHuntPlayerController;
 /**
  * 
  */
@@ -14,4 +16,30 @@ class PROPHUNT_API APropHuntGameState : public AGameStateBase
 {
 	GENERATED_BODY()
 	
+public:
+
+	APropHuntGameState();
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	void AddPlayerController(APropHuntPlayerController* NewController);
+	TArray<APropHuntPlayerController*> GetPlayerControllerList() const;
+
+	void AddHunter(APropHuntPlayerController* NewHunter);
+
+public:
+
+	UPROPERTY(Replicated)
+	TArray<APropHuntPlayerController*> PlayerControllerList;
+
+	UPROPERTY(Replicated)
+	TArray<APropHuntPlayerController*> HunterList;
+
+	UPROPERTY(Replicated)
+	bool bHasGameStarted;
+
+	int32 MinPlayerNum;
+
+	FVector HunterStartLocation;
+
 };
