6ccbc0b3d308dba4081fac1538ac1a1707265d7a
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-12-07 15:55:57 +0530

    Created new map "MenuMap" and its related files.
    This helps in separating menu actions from gameplay actions.

---
+++ Source/PropHunt/Controller/MenuController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MenuController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROPHUNT_API AMenuController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/PropHunt/States/PropHuntPlayerState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "PropHuntPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROPHUNT_API APropHuntPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
