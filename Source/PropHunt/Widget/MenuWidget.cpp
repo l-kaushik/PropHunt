f4f570d313fae23a38c0a2b17492aeb403f76151
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-12-08 15:09:05 +0530

    Implemented components for MainMenu
    Created buttons play game, options and quit game.
    Implemented OnClicked events for button.
    Created a blueprint implementation of this class.

--- Source/PropHunt/Widget/MenuWidget.cpp
+++ Source/PropHunt/Widget/MenuWidget.cpp
@@ -2,4 +2,109 @@
 
 
 #include "MenuWidget.h"
+#include "Components/Button.h"
+#include "Components/VerticalBox.h"
+#include "Components/VerticalBoxSlot.h"
 
+
+void UMenuWidget::NativeConstruct()
+{
+	BindClickEvents();
+	InitializeComponents();
+}
+
+void UMenuWidget::NativePreConstruct()
+{
+	InitializeComponents();
+}
+
+// Delegate binding
+void UMenuWidget::BindClickEvents()
+{
+	if (PlayGameButton)
+	{
+		PlayGameButton->OnClicked.AddDynamic(this, &UMenuWidget::OnPlayGameButtonClicked);
+	}
+
+	if (OptionsButton)
+	{
+		OptionsButton->OnClicked.AddDynamic(this, &UMenuWidget::OnOptionsButtonClicked);
+	}
+
+	if (QuitGameButton)
+	{
+		QuitGameButton->OnClicked.AddDynamic(this, &UMenuWidget::OnQuitGameButtonClicked);
+	}
+}
+
+void UMenuWidget::InitializeComponents()
+{
+	InitializeMainMenuVBox();
+	InitializePlayGameButton();
+	InitializeOptionsButton();
+	InitializeQuitGameButton();
+}
+
+// Functions to initialize components
+
+void UMenuWidget::OnPlayGameButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Play Game Button Clicked!"));
+}
+
+void UMenuWidget::OnOptionsButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Options Button Clicked!"));
+}
+
+void UMenuWidget::OnQuitGameButtonClicked()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Quit Game Button Clicked!"));
+}
+
+// Main Menu UI Components
+
+void UMenuWidget::InitializeMainMenuVBox()
+{
+	if (MainMenuVBox)
+	{
+		MainMenuVBox->AddChild(PlayGameButton);
+		MainMenuVBox->AddChild(OptionsButton);
+		MainMenuVBox->AddChild(QuitGameButton);
+	}
+}
+
+void UMenuWidget::InitializePlayGameButton()
+{
+	if (PlayGameButton)
+	{
+		SetMainMenuButtons(PlayGameButton);
+	}
+}
+
+void UMenuWidget::InitializeOptionsButton()
+{
+	if (OptionsButton)
+	{
+		SetMainMenuButtons(OptionsButton);
+	}
+}
+
+void UMenuWidget::InitializeQuitGameButton()
+{
+	if (QuitGameButton)
+	{
+		SetMainMenuButtons(QuitGameButton);
+
+	}
+}
+
+void UMenuWidget::SetMainMenuButtons(UButton* Button)
+{
+	UVerticalBoxSlot* ButtonSlot = Cast<UVerticalBoxSlot>(Button->Slot);
+	if (ButtonSlot)
+	{
+		ButtonSlot->SetPadding(FMargin(0.0f, 10.0f, 0.0f, 10.0f));
+		ButtonSlot->SetSize(FSlateChildSize(ESlateSizeRule::Fill));
+	}
+}
--- Source/PropHunt/Widget/MenuWidget.h
+++ Source/PropHunt/Widget/MenuWidget.h
@@ -14,4 +14,46 @@ class PROPHUNT_API UMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+private:
+	virtual void NativeConstruct() override;
+	virtual void NativePreConstruct() override;
+
+	void BindClickEvents();
+	void InitializeComponents();
+
+	// OnClicked event functions
+
+	UFUNCTION()
+	void OnPlayGameButtonClicked();
+	
+	UFUNCTION()
+	void OnOptionsButtonClicked();
+
+	UFUNCTION()
+	void OnQuitGameButtonClicked();
+
+	// Functions to initialize components
+	void InitializeMainMenuVBox();
+	void InitializePlayGameButton();
+	void InitializeOptionsButton();
+	void InitializeQuitGameButton();
+
+	// Formatting
+	void SetMainMenuButtons(class UButton* Button);
+
+private:
+
+	// Main Menu Components
+	UPROPERTY(meta = (BindWidget))
+	class UVerticalBox* MainMenuVBox;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* PlayGameButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* OptionsButton;
+
+	UPROPERTY(meta = (BindWidget))
+	class UButton* QuitGameButton;
+
 };
