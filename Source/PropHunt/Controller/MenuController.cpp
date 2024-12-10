f92a2d5945cbd9ec3b06c9397389ba4aaf0df5ce
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-12-10 22:10:41 +0530

    Implement Host Server Functionality in Menu Controller
    - Added AMenuController::HostServer method to handle server hosting requests.
    - Implemented RequestHostServer_Implementation to forward hosting requests to the game mode via IMenuInterface.
    - Included a check to ensure the game mode implements IMenuInterface before calling HostServer.
    - Added logging to handle cases where the game mode retrieval or interface cast fails.

--- Source/PropHunt/Controller/MenuController.cpp
+++ Source/PropHunt/Controller/MenuController.cpp
@@ -3,6 +3,8 @@
 
 #include "MenuController.h"
 #include "../Widget/MenuWidget.h"
+#include "../GameModes/MenuGameMode.h"
+
 #include "Kismet/GameplayStatics.h"
 #include "Blueprint/UserWidget.h"
 #include "UObject/ConstructorHelpers.h"
@@ -30,4 +32,22 @@ void AMenuController::BeginPlay()
 	}
 }
 
+void AMenuController::HostServer(const FString& ServerName, const int32& NumberOfPlayers)
+{
+	RequestHostServer(ServerName, NumberOfPlayers);
+}
+
+void AMenuController::RequestHostServer_Implementation(const FString& ServerName, const int32& NumberOfPlayers)
+{
+	AGameModeBase* CurrentGameMode = UGameplayStatics::GetGameMode(GetWorld());
+	
+	if (IMenuInterface* GameModeInterface = Cast<IMenuInterface>(UGameplayStatics::GetGameMode(GetWorld())))
+	{
+		GameModeInterface->HostServer(ServerName, NumberOfPlayers);
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Failed to get game mode"));
+	}
+}
 
--- Source/PropHunt/Controller/MenuController.h
+++ Source/PropHunt/Controller/MenuController.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "../Interfaces/MenuInterface.h"
 #include "MenuController.generated.h"
 
 /**
@@ -12,7 +13,7 @@
 
 class UMenuWidget;
 UCLASS()
-class PROPHUNT_API AMenuController : public APlayerController
+class PROPHUNT_API AMenuController : public APlayerController, public IMenuInterface
 {
 	GENERATED_BODY()
 
@@ -20,6 +21,14 @@ public:
 	AMenuController();
 
 	virtual void BeginPlay() override;
+
+public:
+	// interface functions
+	virtual void HostServer(const FString& ServerName, const int32& NumberOfPlayers) override;
+
+private:
+	UFUNCTION(Server, Reliable)
+	void RequestHostServer(const FString& ServerName, const int32& NumberOfPlayers);
 
 private:
 	TSubclassOf<UMenuWidget> MenuWidgetBPClassRef;
