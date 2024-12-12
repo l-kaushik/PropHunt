3d9b2e382e89f225310c31ce22edf3815c1fab57
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-12-12 14:36:16 +0530

    Implemented create session function

--- Source/PropHunt/Private/Subsystem/PropHuntSubsystem.cpp
+++ Source/PropHunt/Private/Subsystem/PropHuntSubsystem.cpp
@@ -1,5 +1,73 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PropHuntSubsystem.h"
+#include "Subsystem/PropHuntSubsystem.h"
 
+#include "OnlineSubsystemUtils.h"
+#include "OnlineSessionSettings.h"
+#include "Online/OnlineSessionNames.h"
+
+
+// initializing *SessionCompleteDelegate, basically adding the callback function to it
+// this way of initializing is known as member initializer list
+// the order of initialization is depends on the order of declaration in class
+
+UPropHuntSubsystem::UPropHuntSubsystem()
+	: CreateSessionCompleteDelegate(FOnUpdateSessionCompleteDelegate::CreateUObject(this, &ThisClass::OnCreateSessionCompleted))
+{
+}
+
+void UPropHuntSubsystem::CreateSession(int32 NumPublicConnections, bool IsLANMatch)
+{
+	// return and broadcast false if session interface is not found
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (!SessionInterface.IsValid())
+	{
+		OnCreateSessionCompleteEvent.Broadcast(false);
+		return;
+	}
+
+	// session related settings
+	LastSessionSettings = MakeShareable(new FOnlineSessionSettings());
+	LastSessionSettings->NumPrivateConnections = 0;
+	LastSessionSettings->NumPublicConnections = NumPublicConnections;
+	LastSessionSettings->bAllowInvites = true;
+	LastSessionSettings->bAllowJoinInProgress = true;
+	LastSessionSettings->bAllowJoinViaPresence = true;
+	LastSessionSettings->bAllowJoinViaPresenceFriendsOnly = true;
+	LastSessionSettings->bIsDedicated = false;
+	LastSessionSettings->bUsesPresence = true;
+	LastSessionSettings->bIsLANMatch = IsLANMatch;
+	LastSessionSettings->bShouldAdvertise = true;
+
+	// set map name
+	LastSessionSettings->Set(SETTING_MAPNAME, FString("Your Level Name"), EOnlineDataAdvertisementType::ViaOnlineService);
+
+	// adding delegate to on complete and storing handle as well
+	CreateSessionCompleteDelegateHandle = SessionInterface->AddOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegate);
+
+	// retrives the local player object, attempts to create the session with specified settings
+	// if fails, clears the delegate and broadcast false
+	// set a custom session name, instead of using NAME_GameSession
+	const ULocalPlayer* localPlayer = GetWorld()->GetFirstLocalPlayerFromController();
+	if (!SessionInterface->CreateSession(*localPlayer->GetPreferredUniqueNetId(), NAME_GameSession, *LastSessionSettings))
+	{
+		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+
+		OnCreateSessionCompleteEvent.Broadcast(false);
+	}
+}
+
+// get called when the session creation process completes
+// broadcast the sucess or failure
+void UPropHuntSubsystem::OnCreateSessionCompleted(FName SessionName, bool Successful)
+{
+	const IOnlineSessionPtr SessionInterface = Online::GetSessionInterface(GetWorld());
+	if (SessionInterface)
+	{
+		SessionInterface->ClearOnCreateSessionCompleteDelegate_Handle(CreateSessionCompleteDelegateHandle);
+	}
+
+	OnCreateSessionCompleteEvent.Broadcast(Successful);
+
+}
--- Source/PropHunt/Public/Subsystem/PropHuntSubsystem.h
+++ Source/PropHunt/Public/Subsystem/PropHuntSubsystem.h
@@ -3,15 +3,35 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+#include "Interfaces/OnlineSessionInterface.h"
 #include "Subsystems/GameInstanceSubsystem.h"
+
 #include "PropHuntSubsystem.generated.h"
 
-/**
- * 
- */
+// Delegates for session handling functions
+DECLARE_MULTICAST_DELEGATE_OneParam(FPHOnCreateSessionComplete, bool Successful);
+
+
 UCLASS()
 class PROPHUNT_API UPropHuntSubsystem : public UGameInstanceSubsystem
 {
 	GENERATED_BODY()
+
+public:
+	UPropHuntSubsystem();
+
+	void CreateSession(int32 NumPublicConnections, bool IsLANMatch);
+
+	FPHOnCreateSessionComplete OnCreateSessionCompleteEvent;
+
+protected:
+	void OnCreateSessionCompleted(FName SessionName, bool Successful);
+
+private:
+	TSharedPtr<FOnlineSessionSettings> LastSessionSettings;
+
+	FOnCreateSessionCompleteDelegate CreateSessionCompleteDelegate;
+	FDelegateHandle CreateSessionCompleteDelegateHandle;
 	
 };
