fb7d35f155f9e4c86f12eead5ff18392d2c2094f
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2024-12-29 18:58:16 +0530

    Add utility class for reusable widget functions
    Added WidgetUtils class for common widget operations and utility functions.
    Implemented methods for:
    - Creating and validating widgets (CreateAndValidateWidget).
    - Adding widgets to a UWidgetSwitcher (AddWidgetToWidgetSwitcher).
    - Initializing container widgets like UPanelWidget with child widgets (InitializeContainerBox).
    - Setting padding and size for widget components (SetPaddingAndSize).
    - Included template-based flexibility for different widget and container types.
    - Added safety checks and debug logs for better error handling.

---
+++ Source/PropHunt/Public/Utils/WidgetUtils.h
@@ -0,0 +1,79 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/WidgetSwitcher.h"
+
+/**
+ * 
+ */
+class PROPHUNT_API WidgetUtils
+{
+public:
+	WidgetUtils();
+	~WidgetUtils();
+
+public:
+	// Widget creation methods
+	template<typename WidgetType, typename OwnerType = UObject>
+	static WidgetType* CreateAndValidateWidget(OwnerType Owner, TSubclassOf<UUserWidget> WidgetBPClassRef = WidgetType::StaticClass())
+	{
+		if (!WidgetBPClassRef)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Widget blueprint class reference is invalid"));
+			return nullptr;
+		}
+
+		WidgetType* WidgetRef = CreateWidget<WidgetType>(Owner, WidgetBPClassRef);
+
+		if (!WidgetRef)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Failed to create widget, check template argument type and widget blueprint class reference"));
+			return nullptr;
+		}
+
+		return WidgetRef;
+	}
+
+	template<typename WidgetType, typename WidgetSwitcherT = UWidgetSwitcher, typename OwnerType = UObject>
+	static void AddWidgetToWidgetSwitcher(OwnerType Owner, WidgetSwitcherT* WidgetSwitcher, TSubclassOf<UUserWidget> WidgetBPClassRef = WidgetType::StaticClass())
+	{
+		if (!WidgetBPClassRef)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("WidgetSwitcher object is invalid or null."));
+			return;
+		}
+
+		if (!WidgetBPClassRef)
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Widget blueprint class reference is invalid"));
+			return;
+		}
+
+		WidgetSwitcher->AddChild(CreateAndValidateWidget<WidgetType>(Owner, WidgetBPClassRef));
+	}
+
+
+	// Properties setter templated methods
+	template <typename ContainerType = UPanelWidget, typename ChildType = UUserWidget>
+	static void InitializeContainerBox(ContainerType* Box, ChildType* ChildWidget)
+	{
+		if (Box && ChildWidget)
+		{
+			Box->AddChild(ChildWidget);
+		}
+	}
+
+	template <typename ComponentType, typename SlotType>
+	static void SetPaddingAndSize(ComponentType* Component, FMargin margin, FSlateChildSize ChildSize)
+	{
+		SlotType* ComponentSlot = Cast<SlotType>(Component->Slot);
+		if (ComponentSlot)
+		{
+			ComponentSlot->SetPadding(margin);
+			ComponentSlot->SetSize(ChildSize);
+		}
+	}
+};
