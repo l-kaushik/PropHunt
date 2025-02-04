bd8c58f27b04d5ba311481e6b6fcf7a531c87d72
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-02-04 13:57:06 +0530

    Added UMG module

--- Source/PropHunt/PropHunt.Build.cs
+++ Source/PropHunt/PropHunt.Build.cs
@@ -8,6 +8,6 @@ public class PropHunt : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "OnlineSubsystem", "OnlineSubsystemUtils" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG", "OnlineSubsystem", "OnlineSubsystemUtils" });
 	}
 }
