b12724b021146898dd29b2c82ebc351681824a7e
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-03-14 01:14:58 +0530

    Created GameConstants class A static class responsible for storing constanst used by the game

---
+++ Source/PropHunt/Public/Utils/GameConstants.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROPHUNT_API GameConstants
+{
+public:
+	static constexpr const TCHAR* Map_Menu = TEXT("/Game/ThirdPerson/Maps/MenuMap");
+	static constexpr const TCHAR* Map_ThirdPerson = TEXT("/Game/ThirdPerson/Maps/ThirdPersonMap");
+};
