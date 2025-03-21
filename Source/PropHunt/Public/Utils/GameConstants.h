5cc7b8d3feb27bce16a45a0ce49b25deb5ad1cb0
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-03-21 13:21:42 +0530

    Update GameConstants.h
    - Added warehouse map.
    - Implemented function to get Map with listen command.

--- Source/PropHunt/Public/Utils/GameConstants.h
+++ Source/PropHunt/Public/Utils/GameConstants.h
@@ -12,4 +12,9 @@ class PROPHUNT_API GameConstants
 public:
 	static constexpr const TCHAR* Map_Menu = TEXT("/Game/ThirdPerson/Maps/MenuMap");
 	static constexpr const TCHAR* Map_ThirdPerson = TEXT("/Game/ThirdPerson/Maps/ThirdPersonMap");
+	static constexpr const TCHAR* Map_Warehouse = TEXT("/Game/ThirdPerson/Maps/WarehouseMap");
+
+	static FString GetMapWithListen(const TCHAR* MapName) {
+		return FString::Printf(TEXT("%s?listen"), MapName);
+	}
 };
