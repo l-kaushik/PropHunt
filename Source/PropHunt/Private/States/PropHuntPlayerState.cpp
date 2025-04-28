6c0c24e10032362bdf03c156db14dd373add5ff6
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2025-04-28 22:06:39 +0530

    Implement replication and stat tracking in PropHuntPlayerState
    - Added replication of kills, assists, damage given, damage taken, and longest hidden time using DOREPLIFETIME.
    - Implemented AddKill, AddAssist, AddDamageDealt, and AddDamageTaken functions to update player statistics.
    - Added getters for kills, assists, damage given, and damage taken.
    - Ensured all stat changes are properly replicated across the network.

--- Source/PropHunt/Private/States/PropHuntPlayerState.cpp
+++ Source/PropHunt/Private/States/PropHuntPlayerState.cpp
@@ -3,3 +3,56 @@
 
 #include "States/PropHuntPlayerState.h"
 
+#include "Net/UnrealNetwork.h"
+
+void APropHuntPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(APropHuntPlayerState, m_Kills);
+	DOREPLIFETIME(APropHuntPlayerState, m_Assists);
+	DOREPLIFETIME(APropHuntPlayerState, m_DamageGiven);
+	DOREPLIFETIME(APropHuntPlayerState, m_DamageTaken);
+	DOREPLIFETIME(APropHuntPlayerState, m_LongestHiddenTime);
+}
+
+void APropHuntPlayerState::AddKill()
+{
+	m_Kills++;
+}
+
+void APropHuntPlayerState::AddAssist()
+{
+	m_Assists++;
+}
+
+void APropHuntPlayerState::AddDamageDealt(int32 damageDealt)
+{
+	m_DamageGiven += damageDealt;
+}
+
+void APropHuntPlayerState::AddDamageTaken(int32 damageTaken)
+{
+	m_DamageTaken += damageTaken;
+}
+
+int32 APropHuntPlayerState::GetKills() const
+{
+	return m_Kills;
+}
+
+int32 APropHuntPlayerState::GetAssists() const
+{
+	return m_Assists;
+}
+
+int32 APropHuntPlayerState::GetDamageGiven() const
+{
+	return m_DamageGiven;
+}
+
+int32 APropHuntPlayerState::GetDamageTaken() const
+{
+	return m_DamageTaken;
+}
+
--- Source/PropHunt/Public/States/PropHuntPlayerState.h
+++ Source/PropHunt/Public/States/PropHuntPlayerState.h
@@ -13,5 +13,37 @@ UCLASS()
 class PROPHUNT_API APropHuntPlayerState : public APlayerState
 {
 	GENERATED_BODY()
-	
+
+public:
+
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	void AddKill();
+	void AddAssist();
+	void AddDamageDealt(int32 damageDealt);
+	void AddDamageTaken(int32 damageTaken);
+
+	int32 GetKills() const;
+	int32 GetAssists() const;
+	int32 GetDamageGiven() const;
+	int32 GetDamageTaken() const;
+
+
+	// track the current hidden time, and when player move compare with longest hidden time and update accordingly
+
+private:
+	UPROPERTY(Replicated)
+	int32 m_Kills;
+
+	UPROPERTY(Replicated)
+	int32 m_Assists;
+
+	UPROPERTY(Replicated)
+	int32 m_DamageGiven;
+
+	UPROPERTY(Replicated)
+	int32 m_DamageTaken;
+
+	UPROPERTY(Replicated)
+	double m_LongestHiddenTime;
 };
